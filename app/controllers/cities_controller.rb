ad759ec5a306eafe833deb3129fedba90845d7c6
Lukasz Jeda <ljeda@poczta.fm>
2015-02-28 17:22:04 -0500

    Test Task 6

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,4 +1,5 @@
 class CitiesController < ApplicationController
+  before_filter :authenticate_user!
 
   def index
     @cities = City.paginate(:page => params[:page])
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,6 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+{ 'London' => 'Ld', 'Cracow' => 'Krk', 'Berlin' => 'Ber', 'Rome' => 'Rom', 'Paris' => 'Par', 'Oslo' => 'Os', 'Porto' => 'Por' }.each do |name, short_name|
+  City.new(name: name, short_name: short_name).save if City.find_by(name: name).nil?
+end
