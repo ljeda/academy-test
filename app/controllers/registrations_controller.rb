f15c7773851b8c87ea3df7f62413d7ec90a469fa
Lukasz Jeda <ljeda@poczta.fm>
2015-02-28 13:57:24 -0500

    Test Task 4

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
-    before_filter :configure_permitted_parameters, if: :devise_controller?
+    before_filter :configure_permitted_parameters
 
     protected
 
         def configure_permitted_parameters
-            devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :age, :email, :password, :password_confirmation) }
-            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :age, :email, :password, :password_confirmation, :current_password) }
+            devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :age, :city_id, :email, :password, :password_confirmation) }
+            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :age, :city_id, :email, :password, :password_confirmation, :current_password) }
         end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150228155436) do
+ActiveRecord::Schema.define(version: 20150228182743) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150228155436) do
     t.datetime "updated_at"
     t.string   "name"
     t.integer  "age"
+    t.integer  "city_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
